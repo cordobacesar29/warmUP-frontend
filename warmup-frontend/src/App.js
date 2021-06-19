f9d185aa6aac399457d8a3356b4c61b8fd82ede3
César Córdoba <cordobacesar29@gmail.com>
2021-06-19 19:45:22 -0300

    add <Header/> and <Page/> component into this file

---
+++ warmup-frontend/src/App.js
@@ -0,0 +1,13 @@
+import {Header} from './components/Header/Header';
+import { Page } from './components/Page';
+// add Header and Page component
+function App() {
+  return (
+    <>
+      <Header/>
+      <Page/>
+    </>
+  );
+}
+
+export default App;
