c441c4eca5e993afceb09b971494f22974fa6eeb
César Córdoba <cordobacesar29@gmail.com>
2021-06-19 19:53:11 -0300

    apiHooks

---
+++ warmup-frontend/src/hooks/apiHook.js
@@ -0,0 +1,39 @@
+import { useContext } from 'react';
+
+import PostContext from '../context/PostContext';
+
+import * as postService from '../service/servicios';
+
+export const usePosts = () => {
+    const { posts, setPosts } = useContext(PostContext);
+
+  const addNewPost = ({ body, title }) => {
+    return postService
+      .addOne({ body, title })
+      .then(newPost => setPosts([...posts, newPost]))
+      .catch(err => console.error(err));
+  };
+
+  const updateOnePost = (id, { body, title }) => {
+    return postService
+      .updateOne(id, { body, title })
+      .then(res => setPosts([...posts.map(p => (p.id === Number(id) ? res : p))]))
+      .catch(err => console.error(err));
+  };
+
+  const deleteOnePost = postId => {
+    return postService
+      .deleteOne(postId)
+      .then(() => setPosts([...posts.filter(p => p.id !== postId)]))
+      .catch(err => console.error(err));
+  };
+
+  return {
+    addNewPost,
+    deleteOnePost,
+    posts,
+    setPosts,
+    updateOnePost,
+  };
+};
+
