e16a6281ba3d992917876cbedfde966cc4918fce
César Córdoba <cordobacesar29@gmail.com>
2021-06-20 21:24:22 -0300

    <Home/> component

---
+++ warmup-frontend/src/components/Home.js
@@ -0,0 +1,10 @@
+import { Flex } from "@chakra-ui/layout";
+import { Tabla } from "./Tabla";
+
+export const Home =() => {
+    return (
+        <Flex align='center' justify='center' mt='2rem' p='2rem'>
+             <Tabla/>
+        </Flex>  
+    );    
+};
